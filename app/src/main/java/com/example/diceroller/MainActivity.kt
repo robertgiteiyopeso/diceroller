25e24137c95769f15b7ab9f51ddce866103fc8a2
Robert Gitei <robert.gitei@yopeso.com>
2022-07-14 14:15:01 +0300

    Adds possibility to pick a die with 4-20 sides

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,19 +5,29 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val slider: Slider = findViewById(R.id.slider)
+        slider.addOnChangeListener { cslider, value, fromUser -> changeDie(slider, value, fromUser)  }
+
         val rollButton: Button = findViewById(R.id.roll_Button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { rollDice(slider.value.toInt()) }
     }
 
-    private fun rollDice() {
+    private fun rollDice(size: Int) {
         //Toast.makeText(this, "ai apasat", Toast.LENGTH_SHORT).show()
-        val randomInt = (1..6).random()
+        val randomInt = (1..size).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun changeDie(slider: Slider, value: Float, fromUser: Boolean) {
+        val diceSize: TextView = findViewById(R.id.dice_size)
+        diceSize.setText("Rolling a ${value.toInt()} sided die")
     }
 }
